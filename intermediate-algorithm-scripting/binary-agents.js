13aab80f84ae5f8ad10f1d1169cefb29b779b683
zenthon <946021566@qq.com>
2017-01-02 21:16:56 +0800

    solve 278

---
+++ intermediate-algorithm-scripting/binary-agents.js
@@ -0,0 +1,13 @@
+/**
+ * Created by zenthon on 17/1/2.
+ */
+
+function binaryAgent(str) {
+  function _binaryString2Int(s) {
+    return s.split('').reverse().map((v, i) =>
+    (v.charCodeAt(0) - '0'.charCodeAt(0)) * Math.pow(2, i)).reduce((a, b) => a + b, 0);
+  }
+  return String.fromCharCode(...str.split(' ').map(_binaryString2Int));
+}
+
+binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
